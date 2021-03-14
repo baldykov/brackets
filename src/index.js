041ec014b6472850114dfdfdbdca7980f58b6cac
Дмитрий Балдыков <baldykovdima@yandex.ru>
2021-03-14 22:25:43 +0300

    feat: implement check()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const templates = bracketsConfig.map((pair) => pair[0] + pair[1]);
+
+  let result;
+  do {
+      result = str;
+      for (const template of templates) {
+          str = str.replace(template, "");
+      }
+      if (str === "") {
+          return true;
+      }
+  } while (result !== str);
+
+  return false;
 }
